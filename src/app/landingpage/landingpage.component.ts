fd98a9ec01d9bf039f5ccdabfce06f809299d898
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-06-20 14:08:28 +0200

    ATF-Section Update
    - HTML
    - CSS

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AtfSectionComponent } from './atf-section/atf-section.component';
+import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule, AtfSectionComponent],
+  imports: [CommonModule, AtfSectionComponent, AboutMeSectionComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
