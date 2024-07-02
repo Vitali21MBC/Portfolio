9ea213093ab981d377ff9050c163e5cc71911b39
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-07-02 10:10:07 +0200

    Responsiveness & Burger Menu added

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -13,8 +13,19 @@ export class HeaderComponent {
   aboutMe: string = 'aboutMe';
   mySkills: string = 'mySkills';
   portfolio: string = 'portfolio';
+  contact: string = 'contact';
+  menuOpen: boolean = false;
+  animationState: string = '';
+
+  burgerIcon: string = './../../../../assets/img/icons/burgerMenu.svg';
+  xIcon: string = './../../../../assets/img/icons/mobileMenuCloseFinal.svg';
 
   navigateStart() {
-    window.location.href='#';
+    window.location.href = '#';
   }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
 }
