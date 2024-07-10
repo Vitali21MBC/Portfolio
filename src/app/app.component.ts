018c9025faeec3c52c17d820b7ae95d883a05fb9
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-07-10 16:18:46 +0200

    Optimization
    - Responsiveness
    - Code cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,7 @@ export class AppComponent {
       easing: 'ease-in-out', // default easing for AOS animations
       once: true, // whether animation should happen only once - while scrolling down
       mirror: true, // whether elements should animate out while scrolling past them
-      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+      anchorPlacement: 'bottom-top', // defines which position of the element regarding to window should trigger the animation
     });
   }
 
